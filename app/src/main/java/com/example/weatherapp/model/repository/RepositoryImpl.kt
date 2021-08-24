8cfab7e03aea6a9d91d0b7d908381bca88349459
AndrewSkv <skvoznikovav@yandex.ru>
2021-08-24 15:30:31 +0400

    загрузка погоды из интернета

--- app/src/main/java/com/example/weatherapp/model/repository/RepositoryImpl.kt
+++ app/src/main/java/com/example/weatherapp/model/repository/RepositoryImpl.kt
@@ -1,11 +1,19 @@
 package com.example.weatherapp.model.repository
 
+import com.example.weatherapp.model.WeatherLoader
 import com.example.weatherapp.model.entities.City
 import com.example.weatherapp.model.entities.Weather
 import com.geekbrains.kotlinmvvm.model.repository.Repository
 
 class RepositoryImpl : Repository {
-    override fun getWeatherFromServer() = Weather()
+    override fun getWeatherFromServer(lat: Double, lng: Double): Weather {
+        val dto = WeatherLoader.loadWeather(lat, lng)
+        return Weather(
+            temperature = dto?.fact?.temp ?: 0,
+            feelsLike = dto?.fact?.feels_like ?: 0,
+            condition = dto?.fact?.condition
+        )
+    }
     override fun getWeatherFromLocalStorageRus() = City.getRussianCities()
     override fun getWeatherFromLocalStorageWorld() = City.getWorldCities()
 }
--- app/src/main/java/com/example/weatherapp/ui/viewModels/MainViewModel.kt
+++ app/src/main/java/com/example/weatherapp/ui/viewModels/MainViewModel.kt
@@ -1,14 +1,11 @@
 package com.example.weatherapp.ui.viewModels
 
-import android.util.Log
 import androidx.lifecycle.*
 import com.example.weatherapp.AppState
 import com.geekbrains.kotlinmvvm.model.repository.Repository
-import java.lang.Thread.sleep
 
 class MainViewModel(private val repository: Repository) : ViewModel(), LifecycleObserver {
     private val liveDataToObserve: MutableLiveData<AppState> = MutableLiveData()
-    private val SLEEP_TIME = 1000L
 
     fun getLiveData() = liveDataToObserve
     fun getWeatherFromLocalSourceRus() = getDataFromLocalSource(isRussian = true)
@@ -17,7 +14,6 @@ class MainViewModel(private val repository: Repository) : ViewModel(), Lifecycle
     private fun getDataFromLocalSource(isRussian: Boolean) {
         liveDataToObserve.value = AppState.Loading
         Thread {
-            sleep(SLEEP_TIME)
             liveDataToObserve.postValue(
                 if(isRussian) {
                     AppState.Success(repository.getWeatherFromLocalStorageRus())
